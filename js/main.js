d4fba5fc101121bcf2de4c5565847d509d639274
Mazharenko <mazhor02@gmail.com>
2018-08-29 14:45:25 +0300

    zcxvzxcvzxfiooioioi

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $( document ).ready(function() {
   $('.top-slider').slick({
-    // nextArrow: '<p class="prevArrow-slick"> > </p>',
     nextArrow: '<i class="fa fa-chevron-circle-right nextArrow-slick" aria-hidden="true"></i>',
     prevArrow: '<i class="fa fa-chevron-circle-left prevArrow-slick" aria-hidden="true"></i>',
     dots: true,
     dotsClass: 'slick-dots',
   });
   $('.new-shop__slider').slick({
-    // nextArrow: '<p class="prevArrow-slick"> > </p>',
     nextArrow: '<i class="fa fa-chevron-circle-right nextArrow-slick" aria-hidden="true"></i>',
     prevArrow: '<i class="fa fa-chevron-circle-left prevArrow-slick" aria-hidden="true"></i>',
     dotsClass: 'slick-dots',
@@ -16,7 +14,7 @@ $( document ).ready(function() {
     {
       breakpoint: 890,
       settings: {
-        slidesToShow: 2
+        slidesToShow: 1,
       }
     }
   ]
